5f118309979f7d3e6bf2a295f3bb539bde24814a
Johana Taus <johana.taus@intellisense.io>
2023-10-11 15:47:10 +0200

    Added API call and fetching the results. WIP UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,64 @@
-import logo from "./logo.svg";
+// Import components from react
+import React, { useState } from "react";
+
+// Import CSS
 import "./App.css";
 
 function App() {
+  const [podcast, setPodcast] = useState(); // useState to save the list of podcasts
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Function to fetch the list of podcasts
+  const fetchFavourites = async () => {
+    const result = await fetch(
+      `https://itunes.apple.com/search?term=${searchTerm}&entity=podcast`
+    ); // Make the API call
+    const data = await result.json(); // Change the result into json format
+    setPodcast(data.results); // Save the data in 'podcast'
+  };
+
   return (
     <div className="App">
-      <h1>Song Finder</h1>
       <input
         type="text"
         placeholder="Enter search term"
-        // onChange={(e) => setSearchTerm(e.target.value)}
+        onChange={(e) => setSearchTerm(e.target.value)}
       ></input>
-      <button className="btn">Search</button>
+      <button className="btn" onClick={() => fetchFavourites()}>
+        Search
+      </button>
       <hr></hr>
+
+      {podcast?.length && (
+        <table>
+          <thead>
+            <tr>
+              <th>Artwork</th>
+              <th>Host name</th>
+              <th>Title</th>
+            </tr>
+          </thead>
+          <tbody>
+            {podcast?.map((episode) => {
+              const list = (
+                <>
+                  <tr>
+                    <td>
+                      <img
+                        src={episode.artworkUrl60}
+                        alt={episode.artistName}
+                      />
+                    </td>
+                    <td key={episode.artistId}>{episode.artistName}</td>
+                    <td>{episode.trackCensoredName}</td>
+                  </tr>
+                </>
+              );
+              return list;
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
